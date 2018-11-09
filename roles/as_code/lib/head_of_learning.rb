232d161b3a236d83345658b43f7f035418653033
Luke Morton <lukemorton.dev@gmail.com>
2018-11-09 07:56:58 +0000

    Add beginnings of Head of Learning role

---
+++ roles/as_code/lib/head_of_learning.rb
@@ -0,0 +1,64 @@
+require_relative './shared_expectations/core_engineer_expectations'
+require_relative './shared_expectations/senior_engineer_expectations'
+require_relative './shared_expectations/delivery_lead_expectations'
+
+JobSpec::Role.definition 'Head of Learning' do
+  description <<~DESCRIPTION
+    Our Head of Learning brings together the knowledge of Made Tech and wider industry expertise to ensure we are always learning new skills that benefit our customers.
+
+    We see career progression and core skill attainment as success metrics for this role.
+
+    ## Salary
+
+    This role has a salary of £TBC depending on experience.
+  DESCRIPTION
+
+  salary 00_000..00_000
+
+  # Upskilling / Career Development
+
+  expected 'to successfully onboarding all technical roles',
+    ''
+
+  expected 'to ensure engineers are available to pair in interviews',
+    ''
+
+  expected 'to develop new core skills guided by customer and company need',
+    ''
+
+  expected 'to ensure every engineer is working towards a core skill',
+    ''
+
+  expected 'to coach and nurture team members to improve their engineering and delivery capability',
+    ''
+
+  expected 'to help support engineers meet the engineering standards we expect',
+    ''
+
+  # Academy
+
+  expected 'to organise Made Tech Academy',
+    ''
+
+  expected 'to ensure Academy Engineers are promoted to Engineers',
+    ''
+
+  expected 'to provide Line Management for Academy Engineers',
+    ''
+
+  # Coaching
+
+  expected 'to advise engineering teams on providing mentoring, coaching and training to customers',
+    ''
+
+  expected 'to inspire engineers to coach in our mentoring network',
+    ''
+
+  # Thought leadership
+
+  expected 'to work with marketing to promote our learning efforts',
+    ''
+
+  expected 'to ensure our learning materials are freely available',
+    ''
+end
---
+++ roles/head_of_learning.md
@@ -0,0 +1,63 @@
+# Head of Learning
+
+Our Head of Learning brings together the knowledge of Made Tech and wider industry expertise to ensure we are always learning new skills that benefit our customers.
+
+We see career progression and core skill attainment as success metrics for this role.
+
+## Salary
+
+This role has a salary of £TBC depending on experience.
+
+
+## Expectations
+
+### To successfully onboarding all technical roles
+
+
+
+### To ensure engineers are available to pair in interviews
+
+
+
+### To develop new core skills guided by customer and company need
+
+
+
+### To ensure every engineer is working towards a core skill
+
+
+
+### To coach and nurture team members to improve their engineering and delivery capability
+
+
+
+### To help support engineers meet the engineering standards we expect
+
+
+
+### To organise Made Tech Academy
+
+
+
+### To ensure Academy Engineers are promoted to Engineers
+
+
+
+### To provide Line Management for Academy Engineers
+
+
+
+### To advise engineering teams on providing mentoring, coaching and training to customers
+
+
+
+### To inspire engineers to coach in our mentoring network
+
+
+
+### To work with marketing to promote our learning efforts
+
+
+
+### To ensure our learning materials are freely available
+
